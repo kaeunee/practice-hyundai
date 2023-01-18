29cd8abd732b0ea0d54949947242ee690ed4264b
가은 <kaeun@kaeunui-MacBookAir.local>
2023-01-18 18:36:52 +0900

    [add] 이가은

--- api/router/postRouter.js
+++ api/router/postRouter.js
@@ -6,7 +6,7 @@ import postController from "../controller/postController";
 router.post("/add", postController.add_post); //게시글 추가
 router.get("/all", postController.get_posts); //게시글 전체 조회
 router.put("/update", postController.edit_content); //게시글 내용 수정
-router.delete("/:post_id", postController.delete_post); //게시글 삭제
+router.delete("/:post_id", postController.delete_post); //게시글 삭제..
 
 module.exports = {
   router,
